91971a8a04373ff2724ab4f643f6fe7b3b84b752
Alejandro <alejandrom.am33@gmail.com>
2022-05-28 15:40:16 -0500

    add: tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="App">
-
+      <h1 className=" text-primary text-6xl font-poppins font-semibold">si</h1>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,30 @@ module.exports = {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      transparent: "transparent",
+      gray: "#E5E7EB",
+      primary:{
+        DEFAULT: '#9341DD',
+        ligth: '#C084FC',
+      },
+      text: '#6B7280',
+      darkBg: '#0F172A',
+      boldText: '#111827',
+      white: '#FFFFFF',
+      black: '#000000',
+    },
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"]
+      }
+    },
+    fontWeight: {
+      light: 300,
+      normal: 400,
+      medium: 500,
+      semibold: 600,
+    }
   },
   plugins: [],
 }
