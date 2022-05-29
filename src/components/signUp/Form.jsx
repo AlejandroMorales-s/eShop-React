c14139d5b68853d5ee2ef4b720ff68e1e88797c7
Alejandro <alejandrom.am33@gmail.com>
2022-05-29 15:43:55 -0500

    add: regex to the password

--- src/components/signUp/Form.jsx
+++ src/components/signUp/Form.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, {useRef, useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
 
 export default function Form() {
+
+    const [valid, setValid] = useState(false);
+    const [match, setMatch] = useState(false);
+
+    const password = useRef();
+    const confirmPassword = useRef();
+
+    const regex = /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/
+
+    const validation = () => {
+        const pass = password.current.value;
+
+        regex.test(pass) ? setValid(true) : setValid(false);
+        confirmPassword.current.value !== password.current.value ? setMatch(false) : setMatch(true);
+    }
+    console.log(valid, match); 
+
+    useEffect(()=>{
+        password.current.oninput = validation
+        confirmPassword.current.oninput = validation
+    },[password, confirmPassword])
 
     return (
         <form className='flex flex-col w-100 max-w-45 m-auto'>
@@ -15,11 +36,11 @@ export default function Form() {
             </div>
             <div className='flex flex-col gap-y-0.5 mb-1.5'>
                 <label htmlFor="password" className='font-semibold dark:text-primary-ligth'>Password</label>
-                <input id='password' type="password" placeholder="Password" className='border-primary border-2 rounded px-0.5 h-4.5 shadow-shadow dark:bg-darkBg dark:border-primary-ligth dark:text-gray' />            
+                <input ref={password} id='password' type="password" placeholder="Password" className='border-primary border-2 rounded px-0.5 h-4.5 shadow-shadow dark:bg-darkBg dark:border-primary-ligth dark:text-gray' />            
             </div>
             <div className='flex flex-col gap-y-0.5 mb-1.5'>
                 <label htmlFor="confirmPassword" className='font-semibold dark:text-primary-ligth'>Confirm password</label>
-                <input id='confirmPassword' type="password" placeholder="Password" className='border-primary border-2 rounded px-0.5 h-4.5 shadow-shadow dark:bg-darkBg dark:border-primary-ligth dark:text-gray' />            
+                <input ref={confirmPassword} id='confirmPassword' type="password" placeholder="Password" className='border-primary border-2 rounded px-0.5 h-4.5 shadow-shadow dark:bg-darkBg dark:border-primary-ligth dark:text-gray' />            
             </div>
             <div className='mt-1 flex justify-between items-center'>
                 <Link to='/login'>
