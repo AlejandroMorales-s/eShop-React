d8e62c9255adfc30cd9cc388dc4038288296b7f0
Alejandro <alejandrom.am33@gmail.com>
2022-05-31 21:41:38 -0500

    add: social media icons

--- src/components/login/SocialMedia.jsx
+++ src/components/login/SocialMedia.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { FcGoogle } from 'react-icons/fc';
+import { BsGithub } from 'react-icons/bs';
+import { SiFacebook } from 'react-icons/si';
 
 export default function SocialMedia() {
     return (
         <div>
             <p className=' font-semibold text-center border-b-2 border-gray mt-1.5 dark:text-gray dark:border-gray-grayDark'>or</p>
-            <div className='flex align-middle justify-center pt-2 gap-1'>
-                <p>fb</p>
-                <p>google</p>
+            <div className='flex items-center justify-center gap-2 pt-2'>
+                <a href='https://backendnodejstzuzulcode.uw.r.appspot.com/api/auth/facebook'><SiFacebook className='h-3 w-3'/></a>
+                <a href='https://backendnodejstzuzulcode.uw.r.appspot.com/api/auth/google'><FcGoogle className='h-3 w-3'/></a>
+                <a href='https://backendnodejstzuzulcode.uw.r.appspot.com/api/auth/github'><BsGithub className='h-3 w-3'/></a>
             </div>
         </div>
     )
