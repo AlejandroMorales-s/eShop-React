3762166d28a5054a896724a3ada91ba391089fc1
Alejandro <alejandrom.am33@gmail.com>
2022-05-31 21:44:42 -0500

    add: post axios method

---
+++ src/api/index.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL: "https://backendnodejstzuzulcode.uw.r.appspot.com"
+});
+
+const post = async (url, data) => {
+    try {
+        const response = await instance.post(url, data, {
+            withCredentials: true
+        });
+        return response.data;
+    } catch (error) {
+        throw error.response.data;
+    }
+}
+
+export {
+    post
+}
