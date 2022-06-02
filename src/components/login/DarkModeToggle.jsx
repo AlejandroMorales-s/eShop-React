6337857fa986a44e9791064c05634b6057160a4c
Alejandro <alejandrom.am33@gmail.com>
2022-06-02 17:55:56 -0500

    add: styles to dark mode toggle btn

--- src/components/login/DarkModeToggle.jsx
+++ src/components/login/DarkModeToggle.jsx
@@ -9,24 +9,26 @@ export default function DarkModeToggle() {
         document.getElementById('html').classList.toggle('dark');
     }
     return (
-        <div onClick={darkMode} className="absolute right-3 top-3 flex">
-            <div className=' -translate-y-1'>
-                <p className='text-darkBg font-semibold -translate-x-1'>Dark Mode</p>
-                <p className='text-white font-semibold -translate-x-1'>Light Mode</p>
-            </div>
-            <Switch
-                checked={enabled}
-                onChange={setEnabled}
-                className={`${enabled ? 'bg-primary' : 'bg-primary'}
-                    relative inline-flex h-[27.5px] w-[60px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
-            >
-                <span className="sr-only">Use setting</span>
-                <span
-                    aria-hidden="true"
-                    className={`${enabled ? 'translate-x-9' : 'translate-x-0'}
-                    pointer-events-none inline-block h-[24px] w-[24px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
-                />
-            </Switch>
-        </div>
+
+            <Switch.Group>
+                <div className='absolute right-3 top-1 flex flex-col items-center justify-center'>
+
+                    <Switch.Label className={`${enabled && 'text-gray'} font-medium`}>{enabled ? 'Dark mode' : 'Ligth mode'}</Switch.Label>
+                
+                    <Switch
+                        onClick={darkMode}
+                        checked={enabled}
+                        onChange={setEnabled}
+                        className={`${enabled ? 'bg-primary' : 'bg-primary'}
+                            relative inline-flex h-[27.5px] w-[60px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+                    >
+                        <span
+                            aria-hidden="true"
+                            className={`${enabled ? 'translate-x-9' : 'translate-x-0'}
+                            pointer-events-none inline-block h-[24px] w-[24px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
+                        />
+                    </Switch>
+                </div>
+            </Switch.Group>
     )
 }
