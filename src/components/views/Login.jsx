af160529868b682b34df2e6089be2026850cd056
Alejandro <alejandrom.am33@gmail.com>
2022-06-02 17:56:25 -0500

    add: sign up and login responsive

--- src/components/views/Login.jsx
+++ src/components/views/Login.jsx
@@ -8,9 +8,8 @@ export default function Login() {
     return (
         <>
             <DocumentTitle title="Login" />
-            <div className=' relative w-screen h-screen'>
 
-                <div className=' border-gray border-2 rounded w-95 max-w-65 m-auto p-2 absolute inset-1/4 h-fit dark:bg-darkBg dark:border-gray-grayDark'>
+                <div className=' border-gray border-2 rounded w-95 max-w-65 m-auto p-2 h-fit dark:bg-darkBg dark:border-gray-grayDark'>
                     <div>
                         <h1 className='text-center font-semibold text-primary text-logo dark:text-primary-ligth'>LOGO</h1>
                         <p className='text-center font-semibold my-1 text-bold dark:text-gray'>Login</p>
@@ -18,7 +17,6 @@ export default function Login() {
                         <SocialMedia/>
                     </div>
                 </div>
-            </div>
             <DarkModeToggle/>
         </>
     )
