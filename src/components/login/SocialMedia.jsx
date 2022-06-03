d877285d8dab85108eaf8be37463061dc04c6623
Alejandro <alejandrom.am33@gmail.com>
2022-06-03 00:37:26 -0500

    add: icons colors

--- src/components/login/SocialMedia.jsx
+++ src/components/login/SocialMedia.jsx
@@ -8,9 +8,9 @@ export default function SocialMedia() {
         <div>
             <p className=' font-semibold text-center border-b-2 border-gray mt-1.5 dark:text-gray dark:border-gray-grayDark'>or</p>
             <div className='flex items-center justify-center gap-2 pt-2'>
-                <a href='https://backendnodejstzuzulcode.uw.r.appspot.com/api/auth/facebook'><SiFacebook className='h-3 w-3'/></a>
+                <a href='https://backendnodejstzuzulcode.uw.r.appspot.com/api/auth/facebook'><SiFacebook className='h-3 w-3 text-fb'/></a>
                 <a href='https://backendnodejstzuzulcode.uw.r.appspot.com/api/auth/google'><FcGoogle className='h-3 w-3'/></a>
-                <a href='https://backendnodejstzuzulcode.uw.r.appspot.com/api/auth/github'><BsGithub className='h-3 w-3'/></a>
+                <a href='https://backendnodejstzuzulcode.uw.r.appspot.com/api/auth/github'><BsGithub className='h-3 w-3 dark:text-primary-ligth'/></a>
             </div>
         </div>
     )
