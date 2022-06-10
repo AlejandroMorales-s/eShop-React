5d9bbffa76a6bddfeab2a2b5d5c68df2101fa341
Alejandro <alejandrom.am33@gmail.com>
2022-06-09 21:59:55 -0500

    fix: dark mode

--- src/components/buyProduct/Aside.jsx
+++ src/components/buyProduct/Aside.jsx
@@ -17,27 +17,27 @@ export default function Aside() {
             {product === undefined ? 
                 <div>Loading...</div> 
             :
-                <div className='flex flex-col items-center p-2 gap-2 border-l-2 border-l-gray'>
+                <div className='flex flex-col items-center p-2 gap-2 border-l-2 border-l-gray dark:border-l-gray-grayDark'>
                     <div className='rounded-full h-[150px] overflow-hidden w-[150px]'>
                         <img src={product.image} alt="" srcSet="" className='object-cover w-full h-full'/>
                     </div>
                     <div>
-                        <p className='text-boldText font-medium text-bold text-center'>{product.name}</p>
-                        <p className='text-boldText font-medium text-bold text-center'>Quantity: 1</p>
+                        <p className='text-boldText font-medium text-bold text-center dark:text-white'>{product.name}</p>
+                        <p className='text-boldText font-medium text-bold text-center dark:text-white'>Quantity: 1</p>
                     </div>
-                    <div className='border-y-2 border-y-gray py-2 w-full flex flex-col gap-2'>
+                    <div className='border-y-2 border-y-gray py-2 w-full flex flex-col gap-2 dark:border-y-gray-grayDark'>
                         <div className='flex items-center justify-between'>
-                            <p className='text-text font-medium text-bold'>Product:</p> 
-                            <p className='text-text font-medium text-bold'>{product.price} MXN</p> 
+                            <p className='text-text font-medium text-bold dark:text-gray'>Product:</p> 
+                            <p className='text-text font-medium text-bold dark:text-gray'>{product.price} MXN</p> 
                         </div>
                         <div className='flex items-center justify-between'>
-                            <p className='text-text font-medium text-bold'>Shopping:</p> 
-                            <p className='text-text font-medium text-bold'>$0 MXN</p> 
+                            <p className='text-text font-medium text-bold dark:text-gray'>Shopping:</p> 
+                            <p className='text-text font-medium text-bold dark:text-gray'>$0 MXN</p> 
                         </div>
                     </div>
                     <div className='flex justify-between w-full'>
-                        <p className='text-boldText text-bold font-semibold'>Total:</p>
-                        <p className='text-boldText text-bold font-semibold'>$100 MXN</p>
+                        <p className='text-boldText text-bold font-semibold dark:text-white'>Total:</p>
+                        <p className='text-boldText text-bold font-semibold dark:text-white'>$100 MXN</p>
                     </div>
                 </div>
         }
--- src/components/buyProduct/Shipping.jsx
+++ src/components/buyProduct/Shipping.jsx
@@ -20,24 +20,24 @@ export default function Shipping() {
                 <div className='p-2 flex flex-col gap-4'>
                     <h2 className='font-semibold text-subtitle dark:text-gray'>How would you like to receive or pick up your purchase?</h2>
                     <div className='flex flex-col gap-1'>
-                        <p className='text-boldText font-medium text-bold'>Address</p>
+                        <p className='text-boldText font-medium text-bold dark:text-white'>Address</p>
                         <div className='flex border-2 border-gray items-center gap-1 dark:border-gray-grayDark bg-white dark:bg-darkBg rounded p-1'>
                             <HiOutlineLocationMarker className='text-[60px] text-primary dark:text-primary-ligth'/>
                             <div>
-                                <p className='font-medium text-boldText'>Zip {shoppingAddress.postalCode}</p>
-                                <p className='font-medium text-text'>{`${shoppingAddress.streetAndNumber}, ${shoppingAddress.country}`}</p>
-                                <p className='font-medium text-text'>{`${shoppingAddress.name} - ${shoppingAddress.phoneNumber}`}</p>
+                                <p className='font-medium text-boldText dark:text-white'>Zip {shoppingAddress.postalCode}</p>
+                                <p className='font-medium text-text dark:text-gray'>{`${shoppingAddress.streetAndNumber}, ${shoppingAddress.country}`}</p>
+                                <p className='font-medium text-text dark:text-gray'>{`${shoppingAddress.name} - ${shoppingAddress.phoneNumber}`}</p>
                             </div>
                         </div>
                     </div>
                     <div className='flex flex-col gap-2'>
-                        <p className='text-boldText font-medium text-bold'>Receive purchase</p>
+                        <p className='text-boldText font-medium text-bold dark:text-white'>Receive purchase</p>
                         <div className='hover:border-primary dark:hover:border-primary-ligth transition-all ease-in-out delay-50 cursor-pointer p-1 bg-white rounded dark:bg-darkBg flex justify-between border-2 border-gray dark:border-gray-grayDark'>
-                            <p className='text-text'>Arrives tomorrow</p>
+                            <p className='text-text dark:text-gray'>Arrives tomorrow</p>
                             <p className='text-bold text-green font-semibold'>FREE</p>
                         </div>
                         <div className='hover:border-primary dark:hover:border-primary-ligth transition-all ease-in-out delay-50 cursor-pointer p-1 bg-white rounded dark:bg-darkBg flex justify-between border-2 border-gray dark:border-gray-grayDark'>
-                            <p className='text-text'>Arrives {day + 2}/{month}/{year} </p>
+                            <p className='text-text dark:text-gray'>Arrives {day + 2}/{month}/{year} </p>
                             <p className='text-bold text-green font-semibold'>FREE</p>
                         </div>
                     </div>
