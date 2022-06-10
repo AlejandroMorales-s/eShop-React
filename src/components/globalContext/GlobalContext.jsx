05b2e3fb1d0b4784a9128e6ba9291b0dde484c3e
Alejandro <alejandrom.am33@gmail.com>
2022-06-09 21:48:40 -0500

    add: products in global context

--- src/components/globalContext/GlobalContext.jsx
+++ src/components/globalContext/GlobalContext.jsx
@@ -1,6 +1,71 @@
 import React, {createContext, useState} from 'react';
 
 export const globalContext = createContext();
+const prod = [
+    {
+        id: 1,
+        name: 'Product 1',
+        price: '$100',
+        image: 'https://picsum.photos/200/300',
+        desc: 'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.'
+    },
+    {
+        id: 2,
+        name: 'Product 2',
+        price: '$100',
+        image: 'https://picsum.photos/200/300',
+        desc: 'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.'
+    },
+    {
+        id: 3,
+        name: 'Product 3',
+        price: '$100',
+        image: 'https://picsum.photos/200/300',
+        desc: 'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.'
+    },
+    {
+        id: 4,
+        name: 'Product 4',
+        price: '$100',
+        image: 'https://picsum.photos/200/300',
+        desc: 'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.'
+    },
+    {
+        id: 5,
+        name: 'Product 5',
+        price: '$100',
+        image: 'https://picsum.photos/200/300',
+        desc: 'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.'
+    },
+    {
+        id: 6,
+        name: 'Product 6',
+        price: '$100',
+        image: 'https://picsum.photos/200/300',
+        desc: 'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.'
+    },
+    {
+        id: 7,
+        name: 'Product 7',
+        price: '$100',
+        image: 'https://picsum.photos/200/300',
+        desc: 'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.'
+    },
+    {
+        id: 8,
+        name: 'Product 8',
+        price: '$100',
+        image: 'https://picsum.photos/200/300',
+        desc: 'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.'
+    },
+    {
+        id: 9,
+        name: 'Product 9',
+        price: '$100',
+        image: 'https://picsum.photos/200/300',
+        desc: 'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.'
+    },
+]
 
 export default function GlobalContext({children}) {
 
@@ -15,6 +80,13 @@ export default function GlobalContext({children}) {
     });
     const [wishlist, setWishlist] = useState([]);
     const [shoppingCart, setShoppingCart] = useState([]);
+    const [buyDetails, setBuyDetails] = useState({
+        products: [],
+        shipping: {},
+        payment: {},
+        total: 0
+    });
+    const [products, setProducts] = useState(prod);
 
     return (
         <globalContext.Provider value={{
@@ -28,7 +100,11 @@ export default function GlobalContext({children}) {
             wishlist,
             setWishlist,
             shoppingCart,
-            setShoppingCart
+            setShoppingCart,
+            buyDetails,
+            setBuyDetails,
+            products,
+            setProducts
         }}>
             {children}
         </globalContext.Provider>
