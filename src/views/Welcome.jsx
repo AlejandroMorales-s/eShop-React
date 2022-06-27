9f9027b7984abae542bc01ee22813aacd3660c61
Alejandro <alejandrom.am33@gmail.com>
2022-06-26 22:00:11 -0500

    add: title in welcome component

--- src/views/Welcome.jsx
+++ src/views/Welcome.jsx
@@ -1,4 +1,5 @@
 import React, {useContext, useEffect} from 'react'
+import DocumentTitle from 'react-document-title';
 import { Link, useNavigate } from 'react-router-dom';
 import { get } from '../api';
 import { globalContext } from '../components/globalContext/GlobalContext';
@@ -21,22 +22,25 @@ export default function Welcome() {
     }, [setUser]);
 
     return (
-        <div className='h-[90vh] w-screen flex  items-center justify-center '>
-            <div className='bg-white dark:bg-darkBg border-2 border-gray dark:border-gray-grayDark p-2 w-fit rounded shadow-shadow'>
-                <h1 className='text-center mb-2 font-semibold text-primary text-logo dark:text-primary-light'>eShop</h1>
-                <h2 className='text-center text-title font-semibold mb-2 dark:text-white'>You already have an account?</h2>
-                <div className='flex gap-2 items-center justify-center'>
-                    <Link to='/login' className='cursor-pointer flex flex-col gap-1 hover:shadow-containersShadow rounded hover:translate-x-1 border-2 border-white dark:border-darkBg hover:dark:border-primary-light hover:border-primary hover:-translate-y-1 transition-all ease-in-out delay-50 p-2'>
-                        <img src={happy} alt="" />
-                        <p className='text-center font-medium text-text dark:text-gray'>I have!</p>
-                    </Link>
-                    <Link to='/signup' className='cursor-pointer flex flex-col gap-1 hover:shadow-containersShadow rounded hover:translate-x-1 border-2 border-white dark:border-darkBg hover:dark:border-primary-light hover:border-primary hover:-translate-y-1 transition-all ease-in-out delay-50 p-2'>
-                        <img src={sad} alt="" />
-                        <p className='text-center font-medium text-text dark:text-gray'>Not yet</p>
-                    </Link>
+        <>
+            <DocumentTitle title='eShop'/>
+            <div className='h-[90vh] w-screen flex  items-center justify-center '>
+                <div className='bg-white dark:bg-darkBg border-2 border-gray dark:border-gray-grayDark p-2 w-fit rounded shadow-shadow'>
+                    <h1 className='text-center mb-2 font-semibold text-primary text-logo dark:text-primary-light'>eShop</h1>
+                    <h2 className='text-center text-title font-semibold mb-2 dark:text-white'>You already have an account?</h2>
+                    <div className='flex gap-2 items-center justify-center'>
+                        <Link to='/login' className='cursor-pointer flex flex-col gap-1 hover:shadow-containersShadow rounded hover:translate-x-1 border-2 border-white dark:border-darkBg hover:dark:border-primary-light hover:border-primary hover:-translate-y-1 transition-all ease-in-out delay-50 p-2'>
+                            <img src={happy} alt="" />
+                            <p className='text-center font-medium text-text dark:text-gray'>I have!</p>
+                        </Link>
+                        <Link to='/signup' className='cursor-pointer flex flex-col gap-1 hover:shadow-containersShadow rounded hover:translate-x-1 border-2 border-white dark:border-darkBg hover:dark:border-primary-light hover:border-primary hover:-translate-y-1 transition-all ease-in-out delay-50 p-2'>
+                            <img src={sad} alt="" />
+                            <p className='text-center font-medium text-text dark:text-gray'>Not yet</p>
+                        </Link>
+                    </div>
                 </div>
+                <DarkModeToggle/>
             </div>
-            <DarkModeToggle/>
-        </div>
+        </>
     )
 }
