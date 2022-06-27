bd9611535a60bb4f1207c2d2d06668f8340e2d1d
Alejandro <alejandrom.am33@gmail.com>
2022-06-27 16:52:37 -0500

    add: loader

--- src/components/feed/Products.jsx
+++ src/components/feed/Products.jsx
@@ -14,9 +14,22 @@ export default function Products() {
     return (
         <>
             <div className='grid relative grid-cols-1 sm:grid-cols-4 gap-2 w-95 max-w-[1000px] m-auto'>
-                {products.map(prod => {
-                    return <ProductCard key={prod._id} setShowingModal={setShowingModal} setModalMessage={setModalMessage} product={prod} />
-                })}
+                {products.length > 0 ?
+                    <>
+                        {products.map(prod => {
+                            return <ProductCard key={prod._id} setShowingModal={setShowingModal} setModalMessage={setModalMessage} product={prod} />
+                        })}
+                    </>
+                :   
+                    <div className='absolute flex items-center justify-center w-full top-[200px]'>
+                        <div class="dot-wave">
+                            <div class="dot-wave__dot"></div>
+                            <div class="dot-wave__dot"></div>
+                            <div class="dot-wave__dot"></div>
+                            <div class="dot-wave__dot"></div>
+                        </div>
+                    </div>
+                }
             </div>
             {showingModal && <Modal type={'success'} title={modalMessage.title} desc={modalMessage.message} setShowingModal={setShowingModal}/>}
         </>
