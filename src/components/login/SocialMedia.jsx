0c79ad09a02c78dbd3b47ee2d3e9efe4a9811f1d
Alejandro <alejandrom.am33@gmail.com>
2022-09-13 15:58:51 -0500

    add: login/sign up with social media

--- src/components/login/SocialMedia.jsx
+++ src/components/login/SocialMedia.jsx
@@ -2,15 +2,33 @@ import React from 'react';
 import { FcGoogle } from 'react-icons/fc';
 import { BsGithub } from 'react-icons/bs';
 import { SiFacebook } from 'react-icons/si';
+import { useNavigate } from 'react-router-dom';
+import { providerLogin, signInMethods } from '../../libs/auth';
+import { doc, setDoc } from 'firebase/firestore'
+import { database } from '../../libs/firebase';
 
 export default function SocialMedia() {
+    const navigate = useNavigate();
+    const loginWithProvider = (id) => {
+        providerLogin(id)
+        .then(async (res) => {
+            const docRef = doc(database, 'users', res.user.uid)
+            await setDoc(docRef, {
+                role: 'REGULAR'
+            })
+            navigate('/feed') 
+        })
+        .catch(error => {
+            console.log(error); 
+        })
+    }
     return (
         <div>
             <p className=' font-semibold text-center border-b-2 border-gray mt-1.5 dark:text-gray dark:border-gray-grayDark'>or</p>
             <div className='flex items-center justify-center gap-2 pt-2'>
-                <a href='https://backendnodejstzuzulcode.uw.r.appspot.com/api/auth/facebook'><SiFacebook className='h-3 w-3 text-fb'/></a>
-                <a href='https://backendnodejstzuzulcode.uw.r.appspot.com/api/auth/google'><FcGoogle className='h-3 w-3'/></a>
-                <a href='https://backendnodejstzuzulcode.uw.r.appspot.com/api/auth/github'><BsGithub className='h-3 w-3 dark:text-primary-light'/></a>
+                <SiFacebook onClick={() => loginWithProvider(signInMethods.facebook)} className='h-3 w-3 text-fb'/>
+                <FcGoogle onClick={() => loginWithProvider(signInMethods.google)} className='h-3 w-3'/>
+                <BsGithub onClick={() => loginWithProvider(signInMethods.github)} className='h-3 w-3 dark:text-primary-light'/>
             </div>
         </div>
     )
