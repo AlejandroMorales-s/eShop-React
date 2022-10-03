fadaf5c3c671e141a8ef17a00c659dcce1755366
Alejandro <alejandrom.am33@gmail.com>
2022-10-03 04:48:48 -0500

    fix: images carrousel

--- src/components/productDetails/ProductDetails.jsx
+++ src/components/productDetails/ProductDetails.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, useState, useEffect, useRef} from 'react';
+import React, {useContext, useState, useEffect} from 'react';
 import ReactDocumentTitle from 'react-document-title';
 import { Link, useParams } from 'react-router-dom';
 import { globalContext } from '../../components/globalContext/GlobalContext';
@@ -18,10 +18,6 @@ export default function ProductDetails() {
         const {buyNowQuantity, setBuyNowQuantity} = useContext(globalContext);
         const {history, setHistory} = useContext(globalContext);
         const {user} = useContext(globalContext)
-        
-        //* Refs
-        const imagesContainer = useRef();
-        const img = useRef();
         
         //* State
         const [product, setProduct] = useState()
@@ -45,33 +41,24 @@ export default function ProductDetails() {
                     id: idParams,
                     data
                 })
+                setImagesTotal(data.images.length - 1)
             } catch (error) {
                 console.log(error) 
             }
         }
         
-        const [imagesTotal] = useState(product?.data.images.length - 1);
+        const [imagesTotal, setImagesTotal] = useState();
         const [imagesPosition, setImagesPosition] = useState(0);
 
         //* Image Slider
         const previousImageClick = () => {
-            if(imagesPosition === 0) {
-                img.current.src = product?.data.images[imagesTotal];
-                setImagesPosition(imagesTotal);
-            }else {
-                img.current.src = product?.data.images[imagesPosition - 1];
-                setImagesPosition(imagesPosition - 1);
-            };
+            if(imagesPosition === 0) setImagesPosition(imagesTotal)
+            else setImagesPosition(imagesPosition - 1)
         };
 
         const nextImageClick = () => {
-            if(imagesPosition === imagesTotal) {
-                img.current.src = product?.data.images[0];
-                setImagesPosition(0);
-            }else {
-                img.current.src = product?.data.images[imagesPosition + 1];
-                setImagesPosition(imagesPosition + 1);
-            };
+            if(imagesPosition === imagesTotal) setImagesPosition(0)
+            else setImagesPosition(imagesPosition + 1)
         };
 
         
@@ -163,38 +150,39 @@ export default function ProductDetails() {
                 setInShoppingCart(shoppingCart?.find(item => item.id === idParams));
             })
             .catch(error => console.log(error))
-        }, [inShoppingCart, inWishlist]);
+        }, []);
+
     return (
         <>
-            <ReactDocumentTitle title={product?.name}/>
+            <ReactDocumentTitle title={product?.data.name}/>
             {product === undefined ?
                 <Loader/>
             :
                 <div className='w-95 max-w-130 mx-auto flex justify-center items-center my-5'>
                     <div className='bg-white p-2 flex flex-col sm:grid sm:grid-cols-product w-full shadow-containersShadow rounded gap-2 dark:bg-darkBg'>
-                        <div ref={imagesContainer} className='sm:h-[625px] h-[300px] rounded w-full overflow-hidden relative'>
-                            <img ref={img} src={product?.data.images[0]} className=' w-full h-full object-cover' alt='Product'/>
+                        <div className='sm:h-[625px] h-[300px] rounded w-full overflow-hidden relative'>
+                            <img src={product.data.images[imagesPosition]} className=' w-full h-full object-cover' alt='Product'/>
                             <BsFillArrowRightCircleFill onClick={nextImageClick} className='cursor-pointer text-[55px] absolute top-[50%] text-primary right-1'/>
                             <BsFillArrowRightCircleFill onClick={previousImageClick} className='cursor-pointer rotate-180 text-[55px] absolute top-[50%] text-primary left-1'/>
                         </div>
 
                         <div className='flex flex-col gap-2 overflow-auto'>
                             <div className='flex justify-between items-center'>
-                                <h2 className='m-0 font-semibold text-title dark:text-white'>{product?.data.name}</h2>
+                                <h2 className='m-0 font-semibold text-title dark:text-white'>{product.data.name}</h2>
                                 <AiOutlineHeart onClick={addToWishlist} className={`${inWishlist && 'text-primary'} text-[26px] cursor-pointer hover:text-primary dark:text-gray dark:hover:text-primary-light transition-all ease-out delay-50`}/>
                             </div>
-                            <p className='text-[32.5px] font-semibold dark:text-gray'>${product?.data.price} MXN</p>
+                            <p className='text-[32.5px] font-semibold dark:text-gray'>${product.data.price} MXN</p>
                             <div className='flex flex-col gap-1'>
                                 <div className='flex items-center gap-0.5'>
                                     <TbTruckDelivery className='text-[20px] text-green'/>
                                     <p className='text-green'>
-                                        Arrives <span className='font-semibold'>tomorrow</span> <span className='text-boldText dark:text-gray'>for {product?.price < 100 ? '$99 MXN' : 'FREE'}</span>
+                                        Arrives <span className='font-semibold'>tomorrow</span> <span className='text-boldText dark:text-gray'>for {product.price < 100 ? '$99 MXN' : 'FREE'}</span>
                                     </p>
                                 </div>
                                 <div className='flex items-center gap-0.5'>
                                     <FiTruck className='text-[17px] text-green'/>
                                     <p className='text-green'>
-                                        Arrives <span className='font-semibold'>the day after tomorrow</span> <span className='text-boldText dark:text-gray'>for {product?.price < 100 ? '$99 MXN' : 'FREE'}</span>
+                                        Arrives <span className='font-semibold'>the day after tomorrow</span> <span className='text-boldText dark:text-gray'>for {product.price < 100 ? '$99 MXN' : 'FREE'}</span>
                                     </p>
                                 </div>
                             </div>
@@ -204,14 +192,14 @@ export default function ProductDetails() {
                                 onInput={(e)=>{setBuyNowQuantity(e.target.value)}} type="number" min='1' max='50' defaultValue={buyNowQuantity} />
                             </div>
                             <div className='flex flex-col gap-1'>
-                                <Link to={`/${product?.data._id}/buy-product`}>
+                                <Link to={`/${product.id}/buy-product`}>
                                     <button className={`shadow-shadow px-2 py-1 bg-primary text-white font-semibold rounded border-2 border-primary transition-all hover:bg-transparent hover:text-primary dark:bg-primary-light dark:text-darkBg dark:border-primary-light dark:hover:bg-transparent dark:hover:text-primary-light w-full`} type="submit">Buy now</button>
                                 </Link>
                                 <button onClick={addToCart} className='cursor-pointer bg-white rounded shadow-shadow border-2 border-primary dark:bg-darkBg dark:border-primary-light hover:bg-primary dark:hover:bg-primary-light transition-all ease-in-out delay-50 text-primary font-medium dark:text-primary-light p-1 text-center hover:text-white dark:hover:text-boldText delay-50 h-full w-full' type="submit">{inShoppingCart ? 'Remove from shopping cart' : 'Add to shopping cart'}</button>
                             </div>
                             <div>
                                 <h3 className='text-subtitle font-medium dark:text-white'>Description</h3>
-                                <p className='text-text dark:text-gray'>{product?.data.desc}</p>
+                                <p className='text-text dark:text-gray'>{product.data.desc}</p>
                             </div>
                         </div>
                     </div>
