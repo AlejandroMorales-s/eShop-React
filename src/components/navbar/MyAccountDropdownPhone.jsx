938847562477632da118aeb64ff1ec47e0507cd8
Alejandro <alejandrom.am33@gmail.com>
2023-03-04 17:06:55 -0600

    fix logout button

--- src/components/navbar/MyAccountDropdownPhone.jsx
+++ src/components/navbar/MyAccountDropdownPhone.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Menu, Transition } from "@headlessui/react";
 import { Link, useNavigate } from "react-router-dom";
 //* Icons
@@ -6,39 +6,48 @@ import { BiHistory } from "react-icons/bi";
 import { BsBoxSeam } from "react-icons/bs";
 import { IoMdAddCircleOutline } from "react-icons/io";
 import { AiOutlineMenu, AiOutlineUser, AiOutlineHeart } from "react-icons/ai";
-import { useDispatch, useSelector } from "react-redux";
-import { selectLoggedStatus, logout } from "../../features/user/userSlice";
+import { useDispatch } from "react-redux";
+import { logout } from "../../features/user/userSlice";
 import { auth } from "../../libs/firebase";
 
 export default function MyAccountDropdownPhone({ user }) {
-  const dispatch = useDispatch()
-  const loggedStatus = useSelector(selectLoggedStatus)
+  const dispatch = useDispatch();
 
   const options = [
     {
       title: "My account",
       link: "/account",
-      icon: <AiOutlineUser className="text-primary dark:text-primary-light text-[30px]" />,
+      icon: (
+        <AiOutlineUser className="text-primary dark:text-primary-light text-[30px]" />
+      ),
     },
     {
       title: "Wishlist",
       link: "/account/my-wishlist",
-      icon: <AiOutlineHeart className="text-primary dark:text-primary-light text-[30px]" />,
+      icon: (
+        <AiOutlineHeart className="text-primary dark:text-primary-light text-[30px]" />
+      ),
     },
     {
       title: "Orders",
       link: "/account/orders",
-      icon: <BsBoxSeam className="text-primary dark:text-primary-light text-[30px]" />,
+      icon: (
+        <BsBoxSeam className="text-primary dark:text-primary-light text-[30px]" />
+      ),
     },
     {
       title: "History",
       link: "/account/history",
-      icon: <BiHistory className="text-primary dark:text-primary-light text-[30px]" />,
+      icon: (
+        <BiHistory className="text-primary dark:text-primary-light text-[30px]" />
+      ),
     },
     {
       title: "Add product",
       link: "/add-product",
-      icon: <IoMdAddCircleOutline className="text-primary dark:text-primary-light text-[30px]" />,
+      icon: (
+        <IoMdAddCircleOutline className="text-primary dark:text-primary-light text-[30px]" />
+      ),
     },
   ];
 
@@ -53,26 +62,24 @@ export default function MyAccountDropdownPhone({ user }) {
     document.getElementById("html").classList.toggle("dark");
     document.getElementById("html").classList.toggle("bg-darkBody");
     document.getElementById("html").classList.toggle("bg-lightBg");
-    if (document.getElementById("html").classList.contains("bg-darkBody")) setDark(true);
+    if (document.getElementById("html").classList.contains("bg-darkBody"))
+      setDark(true);
     else setDark(false);
   };
 
-  const logoutHandler = () => dispatch(logout(auth))
-
-  useEffect(() => {
-    if (!loggedStatus) navigate('/')
-  }, [loggedStatus])
+  const logoutHandler = () => dispatch(logout(auth)).then(() => navigate("/"));
 
   return (
     <Menu>
       <Menu.Button className="flex items-center justify-center h-full">
-        <div
-          role="button"
-          tabIndex={0}
-          onKeyDown={menuOpen}
-          onClick={menuOpen}
-        >
-          <AiOutlineMenu className={`${isOpen ? "text-primary dark:text-primary-light" : "text-darkBg dark:text-gray"} text-[30px]`} />
+        <div role="button" tabIndex={0} onKeyDown={menuOpen} onClick={menuOpen}>
+          <AiOutlineMenu
+            className={`${
+              isOpen
+                ? "text-primary dark:text-primary-light"
+                : "text-darkBg dark:text-gray"
+            } text-[30px]`}
+          />
         </div>
       </Menu.Button>
       <Transition
@@ -110,21 +117,27 @@ export default function MyAccountDropdownPhone({ user }) {
               )}
             </Menu.Item>
           ))}
-          <Menu.Item onClick={darkMode} className="cursor-pointer bg-white rounded shadow-shadow w-100 border-2 border-primary dark:bg-darkBg dark:border-primary-light hover:bg-primary dark:hover:bg-primary-light transition-all ease-in-out delay-50">
+          <Menu.Item
+            onClick={darkMode}
+            className="cursor-pointer bg-white rounded shadow-shadow w-100 border-2 border-primary dark:bg-darkBg dark:border-primary-light hover:bg-primary dark:hover:bg-primary-light transition-all ease-in-out delay-50"
+          >
             {({ active }) => (
               <div>
                 <p className="text-primary font-medium dark:text-primary-light p-1 text-center hover:text-white dark:hover:text-boldText transition-all ease-in-out delay-50 h-full w-full">
-                  {dark ? "Light" : "Dark"}
-                  {" "}
-                  mode
+                  {dark ? "Light" : "Dark"} mode
                 </p>
               </div>
             )}
           </Menu.Item>
-          <Menu.Item onClick={logoutHandler} className="cursor-pointer bg-red rounded shadow-containersShadow w-100 hover:bg-transparent border-2 border-red transition-all ease-in-out delay-50">
+          <Menu.Item
+            onClick={logoutHandler}
+            className="cursor-pointer bg-red rounded shadow-containersShadow w-100 hover:bg-transparent border-2 border-red transition-all ease-in-out delay-50"
+          >
             {({ active }) => (
               <div className="">
-                <p className="p-1 text-white font-medium text-center hover:text-red transition-all ease-in-out delay-50 h-full w-full">Log Out</p>
+                <p className="p-1 text-white font-medium text-center hover:text-red transition-all ease-in-out delay-50 h-full w-full">
+                  Log Out
+                </p>
               </div>
             )}
           </Menu.Item>
