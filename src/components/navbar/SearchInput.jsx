9c3a89e1367a21ed727d0260dee2bad1c95ef35f
Alejandro <alejandrom.am33@gmail.com>
2023-03-04 21:16:10 -0600

    fixed filter of products by name in navbar

--- src/components/navbar/SearchInput.jsx
+++ src/components/navbar/SearchInput.jsx
@@ -1,26 +1,28 @@
-import React, { useContext, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import { AiOutlineSearch, AiOutlineClose } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
-import { globalContext } from "../globalContext/GlobalContext";
+import { selectAllProducts } from "../../features/products/productsSlice";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  filterProductsByName,
+  selectProductsFiltered,
+} from "../../features/filters/filtersSlice";
 
 export default function SearchInput() {
-  //* Context
-  const { products } = useContext(globalContext);
-  const { setProductsFiltered } = useContext(globalContext);
-
-  const productsName = products;
+  //* Selectors
+  const productsFiltered = useSelector(selectProductsFiltered);
+  const products = useSelector(selectAllProducts);
 
   //* State
   const [charactersInInput, setCharactersInInput] = useState(0);
   const [showDropdown, setShowDropdown] = useState(false);
-  const [query, setQuery] = useState("");
 
   const inputRef = useRef();
 
   const navigate = useNavigate();
 
-  //* Names of products filtered
-  const filteredProductsName = query === "" ? productsName : productsName.filter((product) => product.data.name.toLowerCase().includes(query.toLowerCase().trim()));
+  //* Dispatch
+  const dispatch = useDispatch();
 
   //* Clear input field
   const clearInput = () => {
@@ -30,27 +32,22 @@ export default function SearchInput() {
 
   //* Search products
   const searchProduct = (e) => {
-    setQuery(inputRef.current.value);
     setShowDropdown(true);
+
+    if (!inputRef.current.value) setShowDropdown(false);
+
     if (e.key === "Enter") {
-      setProductsFiltered(products.filter((product) => {
-        if (product.data.name.toLowerCase().includes(query.toLowerCase().trim())) return product;
-        return null;
-      }));
       setShowDropdown(false);
       navigate("/search-products");
     }
+
+    dispatch(filterProductsByName({ name: e.target.value, products }));
   };
 
-  //* setDropdownValue
-  const setDropdownValue = (e) => {
-    inputRef.current.value = e.target.outerText;
+  //* Open product details
+  const openProductDetails = (id) => {
     setShowDropdown(false);
-    setProductsFiltered(products.filter((product) => {
-      if (product.data.name.toLowerCase().includes(query.toLowerCase().trim())) return product;
-      return null;
-    }));
-    navigate("/search-products");
+    navigate(`/product-details/${id}`);
   };
 
   return (
@@ -64,26 +61,37 @@ export default function SearchInput() {
         placeholder="What are you looking for today?"
       />
       <AiOutlineSearch className="absolute left-[5px] top-[5px] text-[30px] text-text dark:text-gray" />
-      <AiOutlineClose onClick={clearInput} className={`${charactersInInput > 0 ? "absolute" : "hidden"} right-[5px] top-[10px] cursor-pointer text-[17.5px] text-text dark:text-gray`} />
-      <div id="input-dropdown" className={`${charactersInInput > 0 && showDropdown ? "absolute" : "hidden"} w-full p-1 bg-white dark:bg-darkBg border-2 border-gray dark:border-gray-grayDark rounded flex flex-col gap-1 max-h-[275px] overflow-auto`}>
-        {filteredProductsName.length > 0
-          ? (
-            <>
-              {filteredProductsName.map((product) => (
-                <div
-                  role="button"
-                  tabIndex={0}
-                  onKeyDown={setDropdownValue}
-                  onClick={setDropdownValue}
-                  key={product.id}
-                  className="flex p-1 justify-between items-center cursor-pointer hover:bg-primary dark:hover:bg-primary-light rounded hover:text-white dark:hover:text-boldText font-medium text-text dark:text-gray transition-all ease-in-out delay-50"
-                >
-                  <p>{product.data.name}</p>
-                </div>
-              ))}
-            </>
-          )
-          : <p className="font-medium text-center text-text dark:text-gray">Not found</p>}
+      <AiOutlineClose
+        onClick={clearInput}
+        className={`${
+          charactersInInput > 0 ? "absolute" : "hidden"
+        } right-[5px] top-[10px] cursor-pointer text-[17.5px] text-text dark:text-gray`}
+      />
+      <div
+        id="input-dropdown"
+        className={`${
+          showDropdown ? "absolute" : "hidden"
+        } w-full p-1 bg-white dark:bg-darkBg border-2 border-gray dark:border-gray-grayDark rounded flex flex-col gap-1 max-h-[275px] overflow-auto`}
+      >
+        {productsFiltered.length > 0 ? (
+          <>
+            {productsFiltered.map((product) => (
+              <div
+                role="button"
+                tabIndex={0}
+                onClick={() => openProductDetails(product.id)}
+                key={product.id}
+                className="flex p-1 justify-between items-center cursor-pointer hover:bg-primary dark:hover:bg-primary-light rounded hover:text-white dark:hover:text-boldText font-medium text-text dark:text-gray transition-all ease-in-out delay-50"
+              >
+                <p>{product.data.name}</p>
+              </div>
+            ))}
+          </>
+        ) : (
+          <p className="font-medium text-center text-text dark:text-gray">
+            Not found
+          </p>
+        )}
       </div>
     </div>
   );
--- src/views/ProductsFiltered.jsx
+++ src/views/ProductsFiltered.jsx
@@ -1,12 +1,13 @@
-import React, { useContext, useState } from "react";
+import React, { useState } from "react";
 import DocumentTitle from "react-document-title";
-import { globalContext } from "../components/globalContext/GlobalContext";
+import { useSelector } from "react-redux";
 import Modal from "../components/modals/Modal";
 import Navbar from "../components/navbar/Navbar";
 import ProductCard from "../components/productCard/ProductCard";
+import { selectProductsFiltered } from "../features/filters/filtersSlice";
 
 export default function ProductsFiltered() {
-  const { productsFiltered } = useContext(globalContext);
+  const productsFiltered = useSelector(selectProductsFiltered);
   const [showingModal, setShowingModal] = useState(false);
   const [modalMessage, setModalMessage] = useState({
     title: "",
@@ -17,11 +18,8 @@ export default function ProductsFiltered() {
       <DocumentTitle title="Search" />
       <Navbar />
       <h2 className="text-center font-semibold text-title dark:text-gray m-3">
-        {productsFiltered.length}
-        {" "}
-        {productsFiltered.length > 1 ? "products" : "product"}
-        {" "}
-        found
+        {productsFiltered.length}{" "}
+        {productsFiltered.length > 1 ? "products" : "product"} found
       </h2>
       <div className="grid relative grid-cols-1 sm:grid-cols-4 gap-2 w-95 max-w-[1000px] m-auto">
         {productsFiltered.map((product) => (
@@ -33,7 +31,14 @@ export default function ProductsFiltered() {
           />
         ))}
       </div>
-      {showingModal && <Modal type="success" title={modalMessage.title} desc={modalMessage.message} setShowingModal={setShowingModal} />}
+      {showingModal && (
+        <Modal
+          type="success"
+          title={modalMessage.title}
+          desc={modalMessage.message}
+          setShowingModal={setShowingModal}
+        />
+      )}
     </>
   );
 }
