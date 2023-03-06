970b00ec57381f804d1a6acd458c41d86b7d85cc
Alejandro <alejandrom.am33@gmail.com>
2023-03-05 20:39:24 -0600

    removed modal options in wishlist component

--- src/views/Wishlist.jsx
+++ src/views/Wishlist.jsx
@@ -1,18 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import DocumentTitle from "react-document-title";
 import { useSelector } from "react-redux";
 import Breadcrumb from "../components/breadcrumbTrail/Breadcrumb";
-import Modal from "../components/modals/Modal";
 import Navbar from "../components/navbar/Navbar";
 import ProductCard from "../components/productCard/ProductCard";
 import { selectWishlist } from "../features/wishlist/wishlistSlice";
 
 export default function Wishlist() {
-  const [showingModal, setShowingModal] = useState(false);
-  const [modalMessage, setModalMessage] = useState({
-    title: "",
-    message: "",
-  });
   const breadcrumb = [
     {
       link: "/account",
@@ -29,18 +23,14 @@ export default function Wishlist() {
       <DocumentTitle title="Wishlist" />
       <Navbar />
       <Breadcrumb array={breadcrumb} />
-      <h2 className="text-center font-semibold text-title dark:text-gray m-3">My wishlist</h2>
+      <h2 className="text-center font-semibold text-title dark:text-gray m-3">
+        My wishlist
+      </h2>
       <div className="flex flex-col sm:grid sm:grid-cols-4 gap-2 w-95 max-w-[1000px] m-auto">
         {wishlist?.map((product) => (
-          <ProductCard
-            key={product.id}
-            setShowingModal={setShowingModal}
-            setModalMessage={setModalMessage}
-            product={product}
-          />
+          <ProductCard key={product.id} product={product} />
         ))}
       </div>
-      {showingModal && <Modal type="success" title={modalMessage.title} desc={modalMessage.message} setShowingModal={setShowingModal} />}
     </>
   );
 }
